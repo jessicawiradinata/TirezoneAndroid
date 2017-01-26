92c4dab38bdd76210afab9afc81712a3e6adfb11
Jessica <jessicawiradinata@gmail.com>
2017-01-26 11:49:48 +0700

    Transaction details -- Populate cart items (bugs to fix). Populate all details from Firebase.

--- app/src/main/java/id/co/tirezone/tirezonemobile/TransactionDetailsActivity.java
+++ app/src/main/java/id/co/tirezone/tirezonemobile/TransactionDetailsActivity.java
@@ -1,10 +1,14 @@
 package id.co.tirezone.tirezonemobile;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,7 +16,12 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
 
 import java.util.Map;
 
@@ -24,12 +33,16 @@ public class TransactionDetailsActivity extends AppCompatActivity {
 
     private Firebase fSales;
     private Firebase fCustomer;
+    private Firebase fCart;
+    private DatabaseReference itemsRef;
+    private RecyclerView itemsRecyclerView;
 
     private TextView invoiceNoField;
     private TextView nameField;
     private TextView phoneField;
     private TextView vehicleIdField;
     private TextView carField;
+    private TextView totalPriceField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +55,11 @@ public class TransactionDetailsActivity extends AppCompatActivity {
         salesKey = bundle.getString("salesKey");
         customerKey = bundle.getString("customerKey");
         cartKey = bundle.getString("cartKey");
+        Log.v("CART KEY ", cartKey);
         userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        itemsRecyclerView = (RecyclerView) findViewById(R.id.transaction_recyclerview);
+        itemsRef = FirebaseDatabase.getInstance().getReference().child("carts").child(cartKey)
+                .child("items");
 
         fSales = new Firebase("https://tirezonemobile.firebaseio.com/users/" + userId + "/sales/" + salesKey);
         fSales.addValueEventListener(new ValueEventListener() {
@@ -71,6 +88,44 @@ public class TransactionDetailsActivity extends AppCompatActivity {
 
             }
         });
+
+        fCart = new Firebase("https://tirezonemobile.firebaseio.com/carts/" + cartKey);
+        fCart.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String, Integer> map = dataSnapshot.getValue(Map.class);
+                getCartData(map);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Item, TransactionViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Item, TransactionViewHolder>(
+                Item.class,
+                R.layout.item_transaction_product,
+                TransactionViewHolder.class,
+                itemsRef
+        ) {
+            @Override
+            protected void populateViewHolder(TransactionViewHolder viewHolder, Item model, int position) {
+                int count = 0;
+                Log.v("POPULATING", Integer.toString(count++));
+                viewHolder.setPattern(model.getPattern());
+                viewHolder.setSize(model.getSize());
+                viewHolder.setQty(Integer.toString(model.getQty()));
+                viewHolder.setPrice(Integer.toString(model.getSubtotal()));
+            }
+
+        };
+        itemsRecyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 
     @Override
@@ -114,6 +169,7 @@ public class TransactionDetailsActivity extends AppCompatActivity {
         phoneField = (TextView) findViewById(R.id.phone);
         vehicleIdField = (TextView) findViewById(R.id.vehicle_id);
         carField = (TextView) findViewById(R.id.car);
+        totalPriceField = (TextView) findViewById(R.id.total_price);
     }
 
     private void getSalesData(Map<String, String> map) {
@@ -131,5 +187,39 @@ public class TransactionDetailsActivity extends AppCompatActivity {
         phoneField.setText(phone);
         vehicleIdField.setText(vehicleId);
         carField.setText(car);
+    }
+
+    private void getCartData(Map<String, Integer> map) {
+        String totalPrice = Integer.toString(map.get("totalprice"));
+        totalPriceField.setText("Rp " + totalPrice);
+    }
+
+    public static class TransactionViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+
+        public TransactionViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setPattern(String thePattern) {
+            TextView pattern = (TextView) mView.findViewById(R.id.name);
+            pattern.setText(thePattern);
+        }
+
+        public void setSize(String theSize) {
+            TextView size = (TextView) mView.findViewById(R.id.size);
+            size.setText(theSize);
+        }
+
+        public void setQty(String quantity) {
+            TextView qty = (TextView) mView.findViewById(R.id.quantity);
+            qty.setText("Qty " + quantity);
+        }
+
+        public void setPrice(String thePrice) {
+            TextView price = (TextView) mView.findViewById(R.id.price);
+            price.setText("Rp " + thePrice);
+        }
     }
 }
